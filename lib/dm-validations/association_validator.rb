9d411299a39e959c8fe511d2233a040c7790fdbf
roovo <roovo@roovoweb.com>
2009-04-21 17:58:00 +0100

    Initial commit: README contains overview of what I plan

---
+++ lib/dm-validations/association_validator.rb
@@ -0,0 +1,13 @@
+module DataMapper
+  module Validate
+
+    class AssociationValidator
+    end
+
+    module ValidatesAssociation
+
+      def validates_association
+      end
+    end
+  end
+end
