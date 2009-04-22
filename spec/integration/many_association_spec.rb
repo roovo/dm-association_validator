c6e3e993944f2e09a6c2faee61fb5c79817f5862
roovo <roovo@roovoweb.com>
2009-04-22 16:52:24 +0100

    added (already) passing specs for invlaid many associations

--- spec/integration/many_association_spec.rb
+++ spec/integration/many_association_spec.rb
@@ -5,7 +5,7 @@ describe "validates_associated for" do
 
   describe "a valid main and associated model for many associations", :shared => true do
 
-    it "should report the model as valid" do
+    it "should report the main model as VALID" do
       @model.should be_valid
     end
 
@@ -20,7 +20,25 @@ describe "validates_associated for" do
     end
   end
 
-  describe "a VALID association using BASIC association validation", :shared => true do
+  describe "a valid main model with an invalid associated model for many associations", :shared => true do
+
+    it "should report the main model as INVALID" do
+      @model.should_not be_valid
+    end
+
+    it "should NOT have any ERRORS on the main model after a call to valid?" do
+      @model.valid?
+      @model.errors.size.should == 0
+    end
+
+    it "should have ERRORS on the associated model after a call to valid?" do
+      @model.valid?
+      @model.send(@association_reference).first.errors.size.should == 1
+      @model.send(@association_reference).first.errors.on(:name).should_not be_nil
+    end
+  end
+
+  describe "a VALID association using BASIC association validation for many associations", :shared => true do
     
     describe "with a VALID associated model using the BASIC association validations" do
       before(:each) do
@@ -36,7 +54,7 @@ describe "validates_associated for" do
     end
   end
 
-  describe "a VALID association using CONTEXTUAL association validation", :shared => true do
+  describe "a VALID association using CONTEXTUAL association validation for many associations", :shared => true do
     
     describe "with a VALID associated model using the CONTEXTUAL association validations" do
       before(:each) do
@@ -49,6 +67,40 @@ describe "validates_associated for" do
       end
 
       it_should_behave_like "a valid main and associated model for many associations"
+    end
+  end
+
+  describe "an INVALID association using BASIC association validations for many associations", :shared => true do
+
+    describe "with an INVALID associated model using the BASIC association validations" do
+
+      before(:each) do
+        @association_class.validates_present  :name
+        @model_class.validates_association    @association_reference
+
+        @associated_model = @association_class.new
+        @model            = @model_class.new
+        @model.send(@association_reference) << @associated_model
+      end
+
+      it_should_behave_like "a valid main model with an invalid associated model for many associations"
+    end
+  end
+
+  describe "an INVALID association using CONTEXTUAL association validations for many associations", :shared => true do
+
+    describe "with an INVALID associated model using CONTEXTUAL association validations" do
+
+      before(:each) do
+        @association_class.validates_present  :name,                    :when         => [:special_reason]
+        @model_class.validates_association    @association_reference,   :with_context => :special_reason
+
+        @associated_model = @association_class.new
+        @model            = @model_class.new
+        @model.send(@association_reference) << @associated_model
+      end
+
+      it_should_behave_like "a valid main model with an invalid associated model for many associations"
     end
   end
 
@@ -83,7 +135,9 @@ describe "validates_associated for" do
       @association_reference  = :books
     end
     
-    it_should_behave_like "a VALID association using BASIC association validation"
-    it_should_behave_like "a VALID association using CONTEXTUAL association validation"
+    it_should_behave_like "a VALID association using BASIC association validation for many associations"
+    it_should_behave_like "a VALID association using CONTEXTUAL association validation for many associations"
+    it_should_behave_like "an INVALID association using BASIC association validations for many associations"
+    it_should_behave_like "an INVALID association using CONTEXTUAL association validations for many associations"
   end
 end
